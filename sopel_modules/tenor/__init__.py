afc45fb67e2c023df3ef85bffd0b002893b973d7
sharktamer <donohoe.shane@gmail.com>
2019-07-29 22:24:13 +0100

    initialise repo

---
+++ sopel_modules/tenor/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf8
+"""sopel-tenor
+
+A sopel plugin for tenor gifs
+"""
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+from .tenor import *
+
+__author__ = 'Shane Donohoe'
+__email__ = 'shane@donohoe.cc'
+__version__ = '0.1.0'
+
---
+++ sopel_modules/tenor/tenor.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+
+from sopel import module
+
+
+def configure(config):
+    pass
+
+
+def setup(bot):
+    pass
