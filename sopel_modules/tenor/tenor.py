cce5ee05dc06901815835363a8fd96d91770bda1
sharktamer <donohoe.shane@gmail.com>
2019-07-29 22:35:34 +0100

    add function and test for templating api endpoint

--- sopel_modules/tenor/tenor.py
+++ sopel_modules/tenor/tenor.py
@@ -5,11 +5,26 @@ from __future__ import (unicode_literals, absolute_import,
 
 
 from sopel import module
+from sopel.config.types import StaticSection, ValidatedAttribute
+
+import requests
 
 
-def configure(config):
-    pass
+class TenorSection(StaticSection):
+    api = ValidatedAttribute('api_key', str)
 
 
 def setup(bot):
-    pass
+    bot.config.define_section('tenor', TenorSection)
+
+
+def configure(config):
+    config.define_section('api_key', TenorSection)
+    config.tenor.configure_setting('api_key', 'Tenor api key')
+
+
+def template_endpoint(search_term, api_key):
+    endpoint_template = 'https://api.tenor.com/v1/search?q={}&key={}&limit=10'
+    endpoint = endpoint_template.format(search_term, api_key)
+
+    return endpoint
--- tests/test_tenor.py
+++ tests/test_tenor.py
@@ -4,5 +4,14 @@ from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
 
 import unittest
+import unittest.mock
+import pytest
 
 from sopel_modules.tenor import tenor
+
+
+def test_template_endpoint():
+    endpoint = tenor.template_endpoint('test', 'api')
+    expected = 'https://api.tenor.com/v1/search?q=test&key=api&limit=10'
+
+    assert endpoint == expected
